30e0dc212657d30e45c90fc13b0fdfb8f7b4ff19
Nate Pfau <natepfau@yahoo.com>
2021-05-31 12:58:37 -0500

    Added a query parameter to single-repo

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,14 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function()
+{
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    getRepoIssues(repoName);
+    repoNameEl.textContent = repoName;
+};
 
 var getRepoIssues = function (repo) 
 {
@@ -83,4 +92,4 @@ var displayWarning = function (repo)
     limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+getRepoName()
